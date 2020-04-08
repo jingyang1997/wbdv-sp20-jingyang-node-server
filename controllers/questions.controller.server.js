65de3f2df21194d3c0a1f340fc136a9300dc528f
Huskyjjy <jin.jingy@husky.neu.edu>
2020-04-08 17:35:19 -0400

    node server part of hw8

---
+++ controllers/questions.controller.server.js
@@ -0,0 +1,9 @@
+const questionsService = require('../services/questions.service.server')
+module.exports = function (app) {
+    app.get("/api/quizzes/:qzid/questions", (req, res) =>
+        res.json(questionsService.findQuestionsForQuiz(req.params['qid']))
+    )
+    app.get('/api/questions', (req, res) =>
+        res.json(questionsService.findAllQuestions())
+    )
+}
